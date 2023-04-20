fe2e7876b267f334aef6bfd8b195fb6391bb710f
KevinDepedri <kevindepedri@hotmail.it>
2023-04-20 13:20:33 +0200

    Updates and fixes

--- App1_BirthdayCard/app/src/main/java/com/WreckerCompany/FirstAndroidApp/MainActivity.kt
+++ App1_BirthdayCard/app/src/main/java/com/WreckerCompany/FirstAndroidApp/MainActivity.kt
@@ -1,24 +1,18 @@
 package com.WreckerCompany.FirstAndroidApp
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.WreckerCompany.FirstAndroidApp.ui.theme.FirsAndroidAppTheme
 import androidx.compose.ui.unit.dp
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.outlined.Menu
-import androidx.compose.material.icons.rounded.Menu
-import androidx.compose.material.icons.sharp.Menu
-import androidx.compose.material.icons.twotone.Menu
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -31,8 +25,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             FirsAndroidAppTheme {
                 // A surface container using the 'background' color from the theme
+                val context = this
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    BirthdayGreetingWithImage(stringResource(R.string.happy_birthday_text), stringResource(R.string.signature_text))
+                    BirthdayGreetingWithImage(stringResource(R.string.happy_birthday_text), stringResource(R.string.signature_text), context)
                 }
             }
         }
@@ -40,7 +35,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun BirthdayGreetingWithText(message: String, from: String, modifier: Modifier = Modifier) {
+fun BirthdayGreetingWithText(message: String, from: String, context: Context?, modifier: Modifier = Modifier) {
     Column(
         modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Top,
@@ -61,19 +56,27 @@ fun BirthdayGreetingWithText(message: String, from: String, modifier: Modifier =
 
         Spacer(modifier = Modifier.height(550.dp))
 
-        Button(onClick = { println("BUTTON PRESSED") }) {
+        Button(onClick = {
+            if (context != null){
+                sendToast(context)
+            }
+        }) {
             Text(text = stringResource(R.string.open_present_text), fontSize = 24.sp, modifier = Modifier
                 .padding(start = 25.dp, top = 16.dp, end = 25.dp, bottom = 16.dp))
         }
     }
 }
 
+fun sendToast(context: Context){
+    Toast.makeText(context, "HAPPY BIRTHDAY!!", Toast.LENGTH_LONG).show()
+}
+
 @Composable
-fun BirthdayGreetingWithImage(message: String, from: String, modifier: Modifier = Modifier) {
+fun BirthdayGreetingWithImage(message: String, from: String, context: Context? = null, modifier: Modifier = Modifier) {
     val image = painterResource(id = R.drawable.androidparty)
     Box{
         Image(painter = image, contentDescription = null, contentScale = ContentScale.Crop)
-        BirthdayGreetingWithText(message, from, modifier)
+        BirthdayGreetingWithText(message, from, context, modifier)
     }
 }
 
